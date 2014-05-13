d456d9067fa1e660a6c79d4463c2914b1aaf6584
Gert Goet <gert@thinkcreate.nl>
2014-05-13 22:02:39 +0200

    Use drain-tokens and names for processing

--- app.rb
+++ app.rb
@@ -9,24 +9,75 @@ class App < Sinatra::Base
     $amqp_conn.create_channel
   }
 
-=begin
-Logplex headers:
-"HTTP_LOGPLEX_MSG_COUNT"=>"2",
-"HTTP_LOGPLEX_FRAME_ID"=>""
-"HTTP_LOGPLEX_DRAIN_TOKEN"=>"",
-"HTTP_USER_AGENT"=>"Logplex/v72"
-=end
+  before do
+    forbidden_on_unknown_drain_token!
+  end
 
+  # Logplex headers:
+  # HTTP_LOGPLEX_MSG_COUNT"=>"2",
+  # HTTP_LOGPLEX_FRAME_ID"=>""
+  # HTTP_LOGPLEX_DRAIN_TOKEN"=>"",
+  # HTTP_USER_AGENT"=>"Logplex/v72"
   post '/' do
     request.body.rewind
-    body = request.body.readlines
-    drain_token = env["HTTP_LOGPLEX_DRAIN_TOKEN"]
 
     $amqp_channel.with do |ch|
-      body.each do |line|
+      request.body.each do |line|
         log = line.split(/>\d* /).last.to_s.strip
-        ch.topic("bunny-drain").publish(log, routing_key: "heroku.logs.#{drain_token}")
+        puts "Publishing #{log[0...12].inspect} to exchange #{exchange_name.inspect} with routing key: #{routing_key.inspect}"
+        ch.topic(exchange_name).publish(log, routing_key: routing_key)
       end
+    end
+
+    status 201
+  end
+
+  helpers do
+    def exchange_name
+      ENVied.AMQP_EXCHANGE % { drain_name: drain_name, drain_token: drain_token }
+    end
+
+    def routing_key
+      ENVied.AMQP_ROUTING_KEY % { drain_name: drain_name, drain_token: drain_token }
+    end
+
+    # Drain-tokens mapped to drain-names.
+    #
+    # @note drains that you haven't assigned a name in DRAIN_NAME_MAPPING, will
+    # have the drain-token as name. See example below.
+    #
+    # @example
+    #   # Given the following mapping:
+    #   ENV['DRAIN_NAME_MAPPING'] = 'drain-token1=name1&drain-token2='
+    #   drain_name_mapping
+    #   # => {'drain-token1' => 'name1', 'drain-token2' => 'drain-token2'}
+    #
+    # @return [<Hash{String => String}>] the mapping
+    def drain_name_mapping
+      @drain_name_mapping ||= begin
+        ENVied.DRAIN_NAME_MAPPING.inject({}) do |res, (token, name)|
+          res[token] = (name.empty? ? token : name)
+          res
+        end
+      end
+    end
+
+    def drain_token
+      env["HTTP_LOGPLEX_DRAIN_TOKEN"]
+    end
+
+    def drain_name
+      drain_name_mapping[drain_token]
+    end
+
+    def known_drain_token?
+      drain_name_mapping.has_key?(drain_token)
+    end
+  end
+
+  def forbidden_on_unknown_drain_token!
+    unless known_drain_token?
+      halt 403
     end
   end
 end
